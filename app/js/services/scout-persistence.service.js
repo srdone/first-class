1cf05a90eca07d4359417322c28b2b295986a86c
Stephen Done <srdone@gmail.com>
2015-02-05 11:22:37 -0700

    added test data - calcs not working

--- app/js/services/scout-persistence.service.js
+++ app/js/services/scout-persistence.service.js
@@ -27,9 +27,24 @@ app.factory('scoutPersistenceService', function () {
 			completedReqs: [],
 			currentPatrol: "Monkey",
 			troop: '154',
-			positionHistory: [],
-			campingHistory: [],
-			serviceHistory: []
+			positionHistory: [
+				{title: 'WebMaster', start: 'Jan 1, 2013', end: 'Dec 31, 2013'},
+  				{title: 'Patrol Leader', start: 'Jan 1, 2014', end: 'Sep 5, 2014'},
+  				{title: 'Senior Patrol Leader', start: 'Dec 12, 2014'}
+			],
+			serviceHistory: [
+				{description: 'Cleaning the park', hours: 2},
+  				{description: 'Painting the church', hours: 1},
+  				{description: 'Cleaning the ditches', hours: 0.5}
+			],
+			campingHistory: [
+				{description: 'Goblin Valley', start: 'Jan 1, 2012', end: 'Jan 2, 2012'},
+  				{description: 'Deer Creek', start: 'Sep 5, 2013', end: 'Sep 6, 2013'},
+  				{description: 'Zion National Park', start: 'Sep 10, 2014', end: 'Sep 13, 2014'},
+  				{description: 'Maple Dell', start: 'July 10, 2014', end: 'July 15, 2014'},
+  				{description: 'Orange Groves', start: 'Aug 13, 2014', end: 'Aug 16, 2014'},
+  				{description: 'Camporee', start: 'Aug 17, 2014', end: 'Aug 19, 2014'}
+			]
 		},
 		{
 			id: 'testid3',
@@ -40,9 +55,20 @@ app.factory('scoutPersistenceService', function () {
 			completedReqs: [],
 			currentPatrol: "Fox",
 			troop: '174',
-			positionHistory: [],
-			campingHistory: [],
-			serviceHistory: []
+			positionHistory: [
+				{title: 'Patrol Leader', start: 'Jan 23, 2013', end: 'Sep 5, 2014'}
+			],
+			campingHistory: [
+				{description: 'Zion National Park', start: 'Sep 10, 2014', end: 'Sep 13, 2014'},
+  				{description: 'Maple Dell', start: 'July 10, 2014', end: 'July 15, 2014'},
+  				{description: 'Orange Groves', start: 'Aug 13, 2014', end: 'Aug 16, 2014'},
+  				{description: 'Camporee', start: 'Aug 17, 2014', end: 'Aug 19, 2014'}
+			],
+			serviceHistory: [
+				{description: 'Cleaning the park', hours: 2},
+  				{description: 'Painting the church', hours: 1},
+  				{description: 'Cleaning the ditches', hours: 3.5}
+			]
 		}
 	];
 
