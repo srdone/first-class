559a8dd473c3a24fff95471240f941ba48310f27
Stephen Done <srdone@gmail.com>
2015-02-06 11:39:34 -0700

    rough new scout form

--- app/js/controllers/troop.controller.js
+++ app/js/controllers/troop.controller.js
@@ -2,10 +2,29 @@
 
 var app = angular.module('firstClass');
 
-app.controller('TroopController', ['$scope', 'scoutService', function ($scope, scoutService) {
-	$scope.test = 'TroopController';
+app.controller('TroopController', ['$scope', 'scoutService', '$window',
+	function ($scope, scoutService, $window) {
+		$scope.test = 'TroopController';
 
-	$scope.troopNumber = '174';
+		$scope.troopNumber = '174';
 
-	$scope.troop = scoutService.getScoutSummariesInTroop($scope.troopNumber);
+		$scope.newScout = {firstName: '', lastName: ''};
+
+		$scope.troop = scoutService.getScoutSummariesInTroop($scope.troopNumber);
+
+		$scope.addState = false;
+
+		$scope.showAdd = function () {
+			$scope.addState = true;
+		};
+
+		$scope.cancelAdd = function () {
+			$scope.addState = false;
+		};
+
+		$scope.addScout = function () {
+			debugger;
+			$window.alert($scope.newScout.firstName + ' ' + $scope.newScout.lastName);
+		};
+
 }]);
