32eac3cdb76aa2e331c41502ec6cd8cbd3309402
Stephen Done <srdone@gmail.com>
2015-02-06 07:43:12 -0700

    working on scout card

--- app/js/controllers/troop.controller.js
+++ app/js/controllers/troop.controller.js
@@ -5,6 +5,7 @@ var app = angular.module('firstClass');
 app.controller('TroopController', ['$scope', 'scoutService', function ($scope, scoutService) {
 	$scope.test = 'TroopController';
 
-	var troopId = '154';
-	$scope.troop = scoutService.getScoutSummariesInTroop(troopId);
+	$scope.troopNumber = '174';
+
+	$scope.troop = scoutService.getScoutSummariesInTroop($scope.troopNumber);
 }]);
