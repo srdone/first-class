5873d0ed7bfe39503863cd341f17f8b451dc4de0
Stephen Done <srdone@gmail.com>
2015-02-07 14:33:24 -0700

    my first directive

---
+++ app/js/directives/scout-name.directive.js
@@ -0,0 +1,7 @@
+var app = angular.module('firstClass');
+
+app.directive('fcsScoutName', function () {
+	return {
+		templateUrl: '/js/directives/directive-templates/scout-name.template.html'
+	}
+});
