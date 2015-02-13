c1d89337be949af2469bef97bf11dd65503b2e09
Stephen Done <srdone@gmail.com>
2015-02-12 20:56:39 -0700

    added middleware

--- config/express.js
+++ config/express.js
@@ -1,7 +1,23 @@
-var express = require('express');
+var express = require('express'),
+  morgan = require('morgan'),
+  compress = require('compression'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
 
 module.exports = function () {
   var app = express();
+
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  } else if (process.env.NODE_ENV === 'production') {
+    app.use(compress());
+  }
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
 
   require('../app/routes/index.server.routes.js')(app);
 
