232b440ab1da256b6114f7871d15ece86c41a1c3
Stephen Done <srdone@gmail.com>
2015-02-17 14:31:19 -0700

    partially done with adding authentication

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var express = require('express'),
   session = require('express-session'),
   mongoose = require('mongoose'),
   crypto = require('crypto'),
-  methodOverride = require('method-override');
+  methodOverride = require('method-override'),
+  passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 mongoose.connect('mongodb://localhost/first-class-dev');
@@ -48,6 +50,36 @@ UserSchema.methods.authenticate = function (password) {
 
 var User = mongoose.model('User', UserSchema);
 
+//configure passport
+passport.serializeUser = function (user, done) {
+  done(null, user.id);
+};
+
+passport.deserializeUser = function (id, done) {
+  User.findById(id, '-password, -salt', function (err, user) {
+    done(err, user);
+  });
+};
+
+// add local authentication strategy - roughly from MEAN Web Development by PACKT Publishing
+// the "done" callback is used to signal if authentication succeeded:
+// done(err) means there was an error
+// done(null, false) means authentication failed for some reason
+// done(null, user) means authentication succeeded and it takes the user to be added to the session
+passport.use(new LocalStrategy(function (username, password, done) {
+  User.findOne({username: username}).exec(function (err, user) {
+    if (err) {
+      return done(err);
+    } else if (!user) {
+      return done(null, false, {message: 'User ' + username + ' does not exist.'});
+    } else if (!user.authenticate(password)) {
+      return done(null, false, {message: 'Invalid password'});
+    } else {
+      return done(null, user);
+    }
+  });
+}));
+
 // add logging
 app.use(morgan('dev'));
 // parse json data
@@ -64,17 +96,36 @@ app.use(session({
   resave: false //don't save when session has not changed
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// middleware to use on routes to secure them - I got an example of this code from the web
+var requireAuthentication = function (req, res, next) {
+  if (!req.isAuthenticated()) {
+    res.status(401).send({
+      message: 'User not authenticated'
+    });
+  } else {
+    next();
+  }
+};
+
+// middleware to make sure the user owns the document - my idea - how to implement?
+var requireOwnership = function (req, res, next) {
+  //implementation details
+};
+
 // serve the index file
 app.get('/', function (req, res) {
   req.session.lastVisit = Date.now();
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/lastVisit', function (req, res) {
+app.get('/lastVisit', requireAuthentication, function (req, res) {
   res.json({'lastVisit':req.session.lastVisit});
 });
 
-app.route('/auth')
+app.route('/users')
   .post(function (req, res, next) {
     var user = new User(req.body);
 
@@ -121,7 +172,7 @@ app.route('/auth')
   });
 
 
-// server static files
+// serve static files
 app.use('/', express.static(__dirname + '/public/'));
 
 var server = app.listen(3000, function () {
