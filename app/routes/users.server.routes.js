6ba1f7545b0b9ad23d003e6ec09c0f6539cd26d3
Stephen Done <srdone@gmail.com>
2015-02-17 16:36:35 -0700

    modularized user routes

---
+++ app/routes/users.server.routes.js
@@ -0,0 +1,51 @@
+var User = require('mongoose').model('User');
+
+module.exports = function(app) {
+
+  app.route('/users')
+    .post(function (req, res, next) {
+      var user = new User(req.body);
+
+      user.save(function (err) {
+        if (err) {
+          return next(err);
+        } else {
+          res.json(user);
+        }
+      });
+    })
+    .get(function (req, res, next) {
+      User.find({}).exec(function (err, users) {
+        if (err) {
+          return next(err);
+        } else {
+          res.json(users);
+        }
+      });
+    })
+    .delete(function (req, res, next) {
+      User.findOneAndRemove({username: req.body.username}).exec(function (err, user) {
+        if (err) {
+          return next(err);
+        } else if (!user) {
+          res.send({
+            message: 'user does not exist'
+          });
+        } else {
+          res.send({
+            message: 'user ' + user.username + ' successfully removed.'
+          });
+        }
+      });
+    })
+    .put(function (req, res, next) {
+      User.findByIdAndUpdate(req.body._id, req.body).exec(function (err, user) {
+        if (err) {
+          return next(err);
+        } else {
+          res.json(user);
+        }
+      });
+    });
+
+};
--- server.js
+++ server.js
@@ -74,51 +74,8 @@ app.post('/logout', function (req, res) {
   res.send('LoggedOut');
 });
 
-app.route('/users')
-  .post(function (req, res, next) {
-    var user = new User(req.body);
-
-    user.save(function (err) {
-      if (err) {
-        return next(err);
-      } else {
-        res.json(user);
-      }
-    });
-  })
-  .get(function (req, res, next) {
-    User.find({}).exec(function (err, users) {
-      if (err) {
-        return next(err);
-      } else {
-        res.json(users);
-      }
-    });
-  })
-  .delete(function (req, res, next) {
-    User.findOneAndRemove({username: req.body.username}).exec(function (err, user) {
-      if (err) {
-        return next(err);
-      } else if (!user) {
-        res.send({
-          message: 'user does not exist'
-        });
-      } else {
-        res.send({
-          message: 'user ' + user.username + ' successfully removed.'
-        });
-      }
-    });
-  })
-  .put(function (req, res, next) {
-    User.findByIdAndUpdate(req.body._id, req.body).exec(function (err, user) {
-      if (err) {
-        return next(err);
-      } else {
-        res.json(user);
-      }
-    });
-  });
+// add route middleware
+require('./app/routes/users.server.routes')(app);
 
 
 // serve static files
