4efd6784f027f13b426d449f5e77515530f4c4af
Stephen Done <srdone@gmail.com>
2015-02-17 12:34:56 -0700

    simple get and post for user with mongodb

--- server.js
+++ server.js
@@ -1,14 +1,57 @@
 var express = require('express'),
   morgan = require('morgan'),
   bodyParser = require('body-parser'),
-  session = require('express-session');
+  session = require('express-session'),
+  mongoose = require('mongoose'),
+  crypto = require('crypto');
 
 var app = express();
+mongoose.connect('mongodb://localhost/first-class-dev');
+
+//mongoose user schema
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  salt: {
+    type: String
+  }
+});
+
+// this pre save function taken from MEAN Web Development - PACKT Publishing
+UserSchema.pre('save', function (next) {
+  if (this.password) {
+    this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+    this.password = this.hashPassword(this.password);
+  }
+
+  next();
+});
+
+// this hash password method from MEAN Web Developtment - PACKT Publishing
+UserSchema.methods.hashPassword = function (password) {
+  return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+};
+
+// this authentication method from MEAN Web Development - PACKT Publishing
+UserSchema.methods.authenticate = function (password) {
+  return this.password === this.hashPassword(password);
+};
+
+var User = mongoose.model('User', UserSchema);
 
 // add logging
 app.use(morgan('dev'));
 // parse json data
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // configure session
 app.use(session({
@@ -26,6 +69,29 @@ app.get('/', function (req, res) {
 app.get('/lastVisit', function (req, res) {
   res.json({'lastVisit':req.session.lastVisit});
 });
+
+app.route('/auth')
+  .post(function (req, res, next) {
+    var user = new User(req.body);
+
+    user.save(function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        res.json(user);
+      }
+    });
+  })
+  .get(function (req, res, next) {
+    User.find({}).exec(function (err, users) {
+      if (err) {
+        return next(err);
+      } else {
+        res.json(users);
+      }
+    });
+  });
+
 
 // server static files
 app.use('/', express.static(__dirname + '/public/'));
