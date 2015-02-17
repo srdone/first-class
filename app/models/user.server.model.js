25662c23dd084e9c77cbb4e0bf8400e5843b5d26
Stephen Done <srdone@gmail.com>
2015-02-17 16:18:59 -0700

    modularized user model

---
+++ app/models/user.server.model.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose'),
+  crypto = require('crypto'),
+  Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  salt: {
+    type: String
+  }
+});
+
+// this pre save function taken from MEAN Web Development - PACKT Publishing
+UserSchema.pre('save', function (next) {
+  if (this.password) {
+    this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+    this.password = this.hashPassword(this.password);
+  }
+
+  next();
+});
+
+// this hash password method from MEAN Web Developtment - PACKT Publishing
+UserSchema.methods.hashPassword = function (password) {
+  return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+};
+
+// this authentication method from MEAN Web Development - PACKT Publishing
+UserSchema.methods.authenticate = function (password) {
+  return this.password === this.hashPassword(password);
+};
+
+var User = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express'),
   bodyParser = require('body-parser'),
   session = require('express-session'),
   mongoose = require('mongoose'),
-  crypto = require('crypto'),
   methodOverride = require('method-override'),
   passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy;
@@ -11,44 +10,10 @@ var express = require('express'),
 var app = express();
 mongoose.connect('mongodb://localhost/first-class-dev');
 
-//mongoose user schema
-var Schema = mongoose.Schema;
+// import models
+require('./app/models/user.server.model');
 
-var UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  salt: {
-    type: String
-  }
-});
-
-// this pre save function taken from MEAN Web Development - PACKT Publishing
-UserSchema.pre('save', function (next) {
-  if (this.password) {
-    this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
-    this.password = this.hashPassword(this.password);
-  }
-
-  next();
-});
-
-// this hash password method from MEAN Web Developtment - PACKT Publishing
-UserSchema.methods.hashPassword = function (password) {
-  return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
-};
-
-// this authentication method from MEAN Web Development - PACKT Publishing
-UserSchema.methods.authenticate = function (password) {
-  return this.password === this.hashPassword(password);
-};
-
-var User = mongoose.model('User', UserSchema);
+var User = mongoose.model('User');
 
 //configure passport
 passport.serializeUser(function (user, done) {
