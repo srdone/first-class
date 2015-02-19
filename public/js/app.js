19abdc889b88425446b8d41921078b2f3a6c43bd
Stephen Done <srdone@gmail.com>
2015-02-19 13:58:30 -0700

    added ngAnimate to dependencies and added basic css styling

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('firstClass', ['ui.router', 'ui.bootstrap']);
+var app = angular.module('firstClass', ['ui.router', 'ui.bootstrap', 'ngAnimate']);
 
 app.config(['$httpProvider', function ($httpProvider) {
   $httpProvider.interceptors.push('verifyAuthenticationHttpInterceptor');
