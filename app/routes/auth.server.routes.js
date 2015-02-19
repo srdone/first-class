43d97ed8d3d0bd50d0aade1c005a2c665e8ed8e4
Stephen Done <srdone@gmail.com>
2015-02-19 11:19:06 -0700

    added functionality to ask to sign up - sign up doesnt work yet

--- app/routes/auth.server.routes.js
+++ app/routes/auth.server.routes.js
@@ -3,22 +3,8 @@ var passport = require('passport'),
 
 module.exports = function (app) {
 
-  app.post('/login', function (req, res, next) {
-    passport.authenticate('local', function (err, user, info) {
-      if (err) {
-        next(err);
-      } else if (!user) {
-        res.send({message: 'user does not exist', userDoesNotExist: true});
-      } else {
-        req.login(user, function (err) {
-          if (err) {
-            next(err);
-          } else {
-            res.send({message: 'Logged in successfully', username: req.user.username});
-          }
-        })
-      }
-    })(req,res,next);
+  app.post('/login', passport.authenticate('local', {failureFlash: true}), function (req, res) {
+    res.send({message: 'Logged in successfully', username: req.user.username});
   });
 
   app.post('/logout', function (req, res) {
--- public/js/controllers/login.controller.js
+++ public/js/controllers/login.controller.js
@@ -5,13 +5,19 @@ var app = angular.module('firstClass');
 app.controller('LoginController', ['$scope', '$rootScope', 'persistenceService',
 	function ($scope, $rootScope, persistenceService) {
 
-		var login = function () {
-			console.log('hi');
-			persistenceService.login($scope.user.email, $scope.user.password).then(function () {
-        $rootScope.loggedIn = true;
-        $scope.user = {};
-      });
-		};
+    var login = function () {
+      console.log('hi');
+      persistenceService.login($scope.user.email, $scope.user.password)
+        .then(function success () {
+          $scope.message = undefined;
+          $scope.askSignUp = false;
+          $rootScope.loggedIn = true;
+          $scope.user = {};
+        }, function failure (response) {
+          $scope.message = 'Login failed. Are you a member?';
+          $scope.askSignUp = true;
+        });
+    };
 
 		$scope.login = login;
 
