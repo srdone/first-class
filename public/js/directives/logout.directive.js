540d89691dafdfa8007745b1625d8292d8aeb5bd
Stephen Done <srdone@gmail.com>
2015-02-19 08:57:42 -0700

    added logged in $rootScope value change to fcs-logout directive

--- public/js/directives/logout.directive.js
+++ public/js/directives/logout.directive.js
@@ -1,25 +1,28 @@
 'use strict';
 
 angular.module('firstClass').directive('fcsLogout',
-	function () {
+  function () {
 
-		return {
-			restrict: 'A',
-			scope: {
-				title: '@'
-			},
-			controller: function ($scope, $state, persistenceService) {
+    return {
+      restrict: 'A',
+      scope: {
+        title: '@'
+      },
+      controller: ['$scope', '$state', 'persistenceService', '$rootScope',
+        function ($scope, $state, persistenceService, $rootScope) {
 
-				$scope.logout = function () {
-					persistenceService.logout();
-					$state.go('main');
-					console.log("logged out");
-				}
+          $scope.logout = function () {
+            persistenceService.logout().then(function () {
+              $rootScope.loggedIn = false;
+              $state.go('main');
+              console.log("logged out");
+            });
+          }
 
-			},
-			link: function (scope, element, attributes) {
-				element.bind('click', scope.logout);
-			}
-		}
+        }],
+      link: function (scope, element, attributes) {
+        element.bind('click', scope.logout);
+      }
+    }
 
-	});
+  });
