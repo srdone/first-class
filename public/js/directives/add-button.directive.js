8970ca904cb22cd404ee4cdfac53ad84c50bc48b
Stephen Done <srdone@gmail.com>
2015-02-19 16:29:15 -0700

    added add button directive basics

---
+++ public/js/directives/add-button.directive.js
@@ -0,0 +1,7 @@
+angular.module('firstClass').directive('fcsAddButton', function () {
+
+  return {
+    template: '<div class="well well-sm text-center"><span class="glyphicon glyphicon-plus"></span></div>'
+  }
+
+});
