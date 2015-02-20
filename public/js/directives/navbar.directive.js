933d314980bb1e7e66d3193437e671ac49e9a5f6
Stephen Done <srdone@gmail.com>
2015-02-20 11:54:36 -0700

    fixed small navigation issue

--- public/js/directives/navbar.directive.js
+++ public/js/directives/navbar.directive.js
@@ -1,10 +1,18 @@
-angular.module('firstClass').directive('fcsNavbar', ['$rootScope', function($rootScope) {
+angular.module('firstClass').directive('fcsNavbar', ['$rootScope', '$state', function($rootScope, $state) {
   return {
     templateUrl: 'js/directives/directive-templates/navbar.template.html',
     link: function ($scope, $element, $attributes) {
       $rootScope.$watch('username', function (newValue, oldValue) {
         $scope.username = newValue;
       });
+
+      $scope.goHome = function () {
+        if($rootScope.loggedIn) {
+          $state.go('troop');
+        } else {
+          $state.go('main');
+        }
+      };
     }
   }
 }]);
