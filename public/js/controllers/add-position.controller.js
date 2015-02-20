28842ecb52c80a0e3994b4bb59f0f75c680e65f2
Stephen Done <srdone@gmail.com>
2015-02-20 10:34:52 -0700

    added add position

--- public/js/controllers/add-position.controller.js
+++ public/js/controllers/add-position.controller.js
@@ -1,6 +1,25 @@
 angular.module('firstClass').controller('AddPositionController',
   ['$scope', function ($scope) {
 
-    $scope.test = 'Add Position Controller';
+    $scope.addPosition = function () {
+      var service = $scope.scout.addPosition($scope.newPosition.title,
+                                             $scope.newPosition.startDate,
+                                             $scope.newPosition.endDate);
+
+      $scope.scout.save().then(function () {
+        $scope.message = 'Added: ' + $scope.newPosition.title + ': '
+                                   + $scope.newPosition.startDate + ' - '
+                                   + $scope.newPosition.endDate;
+        $scope.newPosition = {};
+      }, function () {
+        $scope.scout.removeService(service.id);
+        $scope.message = 'Error - failed to add service';
+      });
+    };
+
+    $scope.cancelAddService = function () {
+      $scope.newPosition = {};
+      $scope.message = '';
+    }
 
   }]);
