6102b5baa3c813b48bbd20c1e9083e2ae2cfce86
Stephen Done <srdone@gmail.com>
2015-02-20 10:23:43 -0700

    added add service controller

--- public/js/controllers/add-service.controller.js
+++ public/js/controllers/add-service.controller.js
@@ -1,6 +1,21 @@
 angular.module('firstClass').controller('AddServiceController',
   ['$scope', function ($scope) {
 
-    $scope.test = 'Add Service Controller';
+    $scope.addService = function () {
+      var service = $scope.scout.addService($scope.newService.description, $scope.newService.hours);
+
+      $scope.scout.save().then(function () {
+        $scope.message = 'Added: ' + $scope.newService.description + ' - ' + $scope.newService.hours + ' hours';
+        $scope.newService = {};
+      }, function () {
+        $scope.scout.removeService(service.id);
+        $scope.message = 'Error - failed to add service';
+      });
+    };
+
+    $scope.cancelAddService = function () {
+      $scope.newService = {};
+      $scope.message = '';
+    }
 
   }]);
