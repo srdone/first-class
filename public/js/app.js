997256c8ca75d63ccd7c32ec19614b75cb0f25a4
Stephen Done <srdone@gmail.com>
2015-02-20 12:04:49 -0700

    fixed routing error

--- public/js/app.js
+++ public/js/app.js
@@ -10,3 +10,11 @@ app.run(['$rootScope', '$state', function ($rootScope, $state) {
   $rootScope.loggedIn = false;
   $state.go('main');
 }]);
+
+
+app.run(['$state', '$rootScope', function ($state, $rootScope) {
+  $rootScope.$on('$stateChangeError', function (e) {
+    e.preventDefault();
+    $state.go('main');
+  });
+}]);
