9bfe83bcc8993dae0a5664463dd1ab0352eaa4ee
Stephen Done <srdone@gmail.com>
2015-02-28 12:17:27 -0700

    consolidate controllers

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -1,5 +1,10 @@
 'use strict';
 
-angular.module('firstClass').controller('MainController', function($scope) {
+angular.module('firstClass').controller('MainController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
 	$scope.introParagraph = 'Simple, intuitive tracking for your Boy Scout Troop';
-});
+
+  $scope.toggleMenu = function () {
+    $mdSidenav('left').toggle();
+  };
+
+}]);
--- public/js/controllers/navigation.controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('firstClass').controller('NavigationController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
-
-  $scope.toggleMenu = function () {
-    $mdSidenav('left').toggle();
-  };
-
-}]);
