d3cdc7bea2ef8fd6bba04ee1bc1e5021c2b59663
Stephen Done <srdone@gmail.com>
2015-03-02 21:29:32 -0700

    add go to troop link

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -1,10 +1,15 @@
 'use strict';
 
-angular.module('firstClass').controller('MainController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+angular.module('firstClass').controller('MainController', ['$scope', '$mdSidenav', '$state', function($scope, $mdSidenav, $state) {
 	$scope.introParagraph = 'Simple, intuitive tracking for your Boy Scout Troop';
 
   $scope.toggleMenu = function () {
     $mdSidenav('left').toggle();
+  };
+
+  $scope.goToTroopView = function () {
+    $mdSidenav('left').toggle();
+    $state.go('troop');
   };
 
 }]);
