f8be821f3f87b8dab77cbe052ec9c26a8b2d7c3b
Stephen Done <srdone@gmail.com>
2015-03-03 18:33:24 -0700

    indent

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -1,25 +1,25 @@
 'use strict';
 
 angular.module('firstClass').controller('MainController', ['$scope', '$mdSidenav', '$state', '$log', 'persistenceService', '$rootScope',
-    function($scope, $mdSidenav, $state, $log, persistenceService, $rootScope) {
-	$scope.introParagraph = 'Simple, intuitive tracking for your Boy Scout Troop';
+  function($scope, $mdSidenav, $state, $log, persistenceService, $rootScope) {
+    $scope.introParagraph = 'Simple, intuitive tracking for your Boy Scout Troop';
 
-  $scope.toggleMenu = function () {
-    $mdSidenav('left').toggle();
-  };
-
-  $scope.goToTroopView = function () {
-    $mdSidenav('left').toggle();
-    $state.go('troop');
-  };
-
-  $scope.logout = function () {
-    persistenceService.logout().then(function () {
+    $scope.toggleMenu = function () {
       $mdSidenav('left').toggle();
-      $rootScope.loggedIn = false;
-      $state.go('main');
-      $log.debug("logged out");
-    });
-  }
+    };
 
-}]);
+    $scope.goToTroopView = function () {
+      $mdSidenav('left').toggle();
+      $state.go('troop');
+    };
+
+    $scope.logout = function () {
+      persistenceService.logout().then(function () {
+        $mdSidenav('left').toggle();
+        $rootScope.loggedIn = false;
+        $state.go('main');
+        $log.debug("logged out");
+      });
+    }
+
+  }]);
