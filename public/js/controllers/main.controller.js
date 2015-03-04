43adb8a6fb9ec42027aa57d009b6de7a6a54b52e
Stephen Done <srdone@gmail.com>
2015-03-03 18:33:06 -0700

    extracted logout functionality to controller from directive

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
-angular.module('firstClass').controller('MainController', ['$scope', '$mdSidenav', '$state', function($scope, $mdSidenav, $state) {
+angular.module('firstClass').controller('MainController', ['$scope', '$mdSidenav', '$state', '$log', 'persistenceService', '$rootScope',
+    function($scope, $mdSidenav, $state, $log, persistenceService, $rootScope) {
 	$scope.introParagraph = 'Simple, intuitive tracking for your Boy Scout Troop';
 
   $scope.toggleMenu = function () {
@@ -11,5 +12,14 @@ angular.module('firstClass').controller('MainController', ['$scope', '$mdSidenav
     $mdSidenav('left').toggle();
     $state.go('troop');
   };
+
+  $scope.logout = function () {
+    persistenceService.logout().then(function () {
+      $mdSidenav('left').toggle();
+      $rootScope.loggedIn = false;
+      $state.go('main');
+      $log.debug("logged out");
+    });
+  }
 
 }]);
