0494a1a9e80e55dd5f848590f48a6dc8900f9024
Stephen Done <srdone@gmail.com>
2015-03-03 20:03:53 -0700

    remove old modal code

--- public/js/controllers/scout.controller.js
+++ public/js/controllers/scout.controller.js
@@ -7,21 +7,6 @@ app.controller('ScoutController', ['$scope', '$modal', 'scoutService', 'scout', 
 
 		$scope.scout = scout;
 
-    $scope.open = function () {
-
-      var modalInstance = $modal.open({
-        templateUrl: 'modals/add-details.modal.html',
-        controller: 'AddDetailsModalController',
-        size: 'sm',
-        resolve: {
-          scout: function () {
-            return $scope.scout;
-          }
-        }
-      });
-
-    };
-
     $scope.openAddScoutDetailsSheet = function () {
       $mdBottomSheet.show({
         templateUrl: 'js/bottom-sheets/add-details.bottom-sheet.html',
