2998b94bdb081b0dc3fc7ce96973456c1a0beca7
Stephen Done <srdone@gmail.com>
2015-03-07 11:06:26 -0700

    delete button directive

---
+++ public/js/directives/delete-button.directive.js
@@ -0,0 +1,8 @@
+angular.module('firstClass').directive('fcsDeleteButton', function () {
+
+  return {
+    restrict: 'E',
+    templateUrl: 'js/directives/directive-templates/delete-button.template.html'
+  }
+
+});
