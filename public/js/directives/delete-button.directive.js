7120a4da4849fb3487594c191310134da328bd1e
Stephen Done <srdone@gmail.com>
2015-03-07 11:09:59 -0700

    fixed aria string

--- public/js/directives/delete-button.directive.js
+++ public/js/directives/delete-button.directive.js
@@ -2,6 +2,9 @@ angular.module('firstClass').directive('fcsDeleteButton', function () {
 
   return {
     restrict: 'E',
+    scope: {
+      fcsAriaLabel: '@'
+    },
     templateUrl: 'js/directives/directive-templates/delete-button.template.html'
   }
 
