0a469a41b51a6e6ac7922357ed8aedc263f4a218
Stephen Done <srdone@gmail.com>
2015-03-07 11:13:32 -0700

    added transclusion

--- public/js/directives/completed-requirement-summary.directive.js
+++ public/js/directives/completed-requirement-summary.directive.js
@@ -5,6 +5,7 @@ angular.module('firstClass').directive('fcsCompletedRequirementSummary', functio
     scope: {
       completedRequirement: '='
     },
+    transclude: true,
     templateUrl: 'js/directives/directive-templates/completed-requirement-summary.template.html'
   }
 
