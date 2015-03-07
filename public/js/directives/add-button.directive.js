13d4305b9017b6410f6dd36df3cd5aaa573ff051
Stephen Done <srdone@gmail.com>
2015-03-07 14:18:28 -0700

    polish add button

--- public/js/directives/add-button.directive.js
+++ public/js/directives/add-button.directive.js
@@ -2,6 +2,9 @@ angular.module('firstClass').directive('fcsAddButton', function () {
 
   return {
     restrict: 'E',
+    scope: {
+      tipText: '='
+    },
     templateUrl: 'js/directives/directive-templates/add-button.template.html'
   }
 
