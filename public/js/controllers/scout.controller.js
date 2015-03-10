31126d5b28db29d6568edb2b7092a52726288322
Stephen Done <srdone@gmail.com>
2015-03-10 12:25:38 -0600

    completed issue #8 - dialog on delete

--- public/js/controllers/scout.controller.js
+++ public/js/controllers/scout.controller.js
@@ -2,10 +2,12 @@
 
 var app = angular.module('firstClass');
 
-app.controller('ScoutController', ['$scope', 'scoutService', 'scout', '$mdBottomSheet', 'requirementService', '$mdDialog',
-	function ($scope, scoutService, scout, $mdBottomSheet, requirementService, $mdDialog) {
+app.controller('ScoutController', ['$scope', 'scoutService', 'scout', '$mdBottomSheet', 'requirementService', '$mdDialog', '$filter',
+	function ($scope, scoutService, scout, $mdBottomSheet, requirementService, $mdDialog, $filter) {
 
 		$scope.scout = scout;
+
+    var warningDialog = $mdDialog.confirm().title('Warning').ok('Yes').cancel('Cancel');
 
     $scope.openAddScoutDetailsSheet = function () {
       $mdBottomSheet.show({
@@ -38,10 +40,7 @@ app.controller('ScoutController', ['$scope', 'scoutService', 'scout', '$mdBottom
     };
 
     $scope.deleteRequirement = function(completedRequirement) {
-      var dialog = $mdDialog.confirm()
-          .title('Warning').content('Delete requirement ' + completedRequirement.requirement.name + ' and all parents?')
-        .ok('Yes')
-        .cancel('Cancel');
+      var dialog = warningDialog.content('Delete requirement ' + completedRequirement.requirement.name + ' and all parents?');
 
       $mdDialog.show(dialog).then(function () {
         $scope.scout.removeRequirementById(completedRequirement.requirement.id);
@@ -52,18 +51,32 @@ app.controller('ScoutController', ['$scope', 'scoutService', 'scout', '$mdBottom
     };
 
     $scope.deleteCampout = function (campout) {
-      $scope.scout.removeCampout(campout.id);
-      $scope.scout.save();
+      var dialog = warningDialog.content('Delete campout record: ' + campout.description + ': ' +
+        $filter('date')(campout.start, 'shortDate') + '-' + $filter('date')(campout.end, 'shortDate') + '?');
+
+      $mdDialog.show(dialog).then(function () {
+        $scope.scout.removeCampout(campout.id);
+        $scope.scout.save();
+      });
     };
 
     $scope.deleteService = function (service) {
-      $scope.scout.removeService(service.id);
-      $scope.scout.save();
+      var dialog = warningDialog.content('Delete service record: ' + service.description + ': ' + service.hours + ' hours?');
+
+      $mdDialog.show(dialog).then(function () {
+        $scope.scout.removeService(service.id);
+        $scope.scout.save();
+      });
     };
 
     $scope.deletePosition = function (position) {
-      $scope.scout.removePosition(position.id);
-      $scope.scout.save();
+      var dialog = warningDialog.content('Delete position record: ' + position.title + ': ' +
+        $filter('date')(position.start, 'shortDate') + '-' + $filter('date')(position.end, 'shortDate') + '?');
+
+      $mdDialog.show(dialog).then(function () {
+        $scope.scout.removePosition(position.id);
+        $scope.scout.save();
+      });
     };
 
     $scope.updateScout = function () {
