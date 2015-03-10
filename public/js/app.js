e6328ccde97b9ccb7fe8e5dacb362e7d59c3c7be
Stephen Done <srdone@gmail.com>
2015-03-10 07:22:35 -0600

    change state logging to $log.debug

--- public/js/app.js
+++ public/js/app.js
@@ -28,10 +28,10 @@ app.run(function(editableOptions) {
 
 // http://stackoverflow.com/questions/27299471/angular-ui-router-resolve-service-halting-route-change
 // Adds state change hooks; logs to console.
-app.run(function($rootScope, $state) {
+app.run(['$rootScope', '$state', '$log', function($rootScope, $state, $log) {
   $rootScope.$state = $state;
   function message(to, toP, from, fromP) { return from.name  + angular.toJson(fromP) + " -> " +     to.name + angular.toJson(toP); }
-  $rootScope.$on("$stateChangeStart", function(evt, to, toP, from, fromP) { console.log("Start:   " + message(to, toP, from, fromP)); });
-  $rootScope.$on("$stateChangeSuccess", function(evt, to, toP, from, fromP) { console.log("Success: " + message(to, toP, from, fromP)); });
-  $rootScope.$on("$stateChangeError", function(evt, to, toP, from, fromP, err) {     console.log("Error:   " + message(to, toP, from, fromP), err); });
-});
+  $rootScope.$on("$stateChangeStart", function(evt, to, toP, from, fromP) { $log.debug("Start:   " + message(to, toP, from, fromP)); });
+  $rootScope.$on("$stateChangeSuccess", function(evt, to, toP, from, fromP) { $log.debug("Success: " + message(to, toP, from, fromP)); });
+  $rootScope.$on("$stateChangeError", function(evt, to, toP, from, fromP, err) {     $log.debug("Error:   " + message(to, toP, from, fromP), err); });
+}]);
