881012947e514b788d098a2df89e27f2845344f3
Stephen Done <srdone@gmail.com>
2015-03-10 19:54:44 -0600

    improve layout

--- public/js/directives/scout-avatar.directive.js
+++
@@ -1,7 +0,0 @@
-angular.module('firstClass').directive('fcsScoutAvatar', function () {
-
-  return {
-    templateUrl: 'js/directives/directive-templates/scout-avatar.template.html'
-  }
-
-});
---
+++ public/js/directives/scout-summary.directive.js
@@ -0,0 +1,10 @@
+angular.module('firstClass').directive('fcsScoutSummary', function () {
+
+  return {
+    scope: {
+      scout: '='
+    },
+    templateUrl: 'js/directives/directive-templates/scout-summary.template.html',
+  }
+
+});
