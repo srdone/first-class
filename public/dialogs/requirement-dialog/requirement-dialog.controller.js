dfd0d2d981fd27c45ecb268266070b3259cdebcc
Stephen Done <srdone@gmail.com>
2015-03-17 18:33:02 -0600

    fixing formatting

--- public/dialogs/requirement-dialog/requirement-dialog.controller.js
+++ public/dialogs/requirement-dialog/requirement-dialog.controller.js
@@ -3,48 +3,48 @@
 angular.module('firstClass').controller('RequirementDialogController', ['$mdDialog', 'existingRequirements', '$timeout',
   function ($mdDialog, existingRequirements, $timeout) {
 
-  var _init = function () {
-    if (this.preSelectedRequirements.length) {
-      this.preSelectedRequirements.forEach(function markAsSelected(current) {
-        var index = existingRequirements.indexOf(current);
+    var _init = function () {
+      if (this.preSelectedRequirements.length) {
+        this.preSelectedRequirements.forEach(function markAsSelected(current) {
+          var index = existingRequirements.indexOf(current);
 
-        this.existingRequirements[index].isSelected = true;
+          this.existingRequirements[index].isSelected = true;
+        });
+      }
+    };
+
+    $timeout(_init.bind(this), 4);
+
+    var _getAllSelected = function () {
+      return existingRequirements.filter(function (currentRequirement) {
+        return currentRequirement.isSelected;
       });
-    }
-  };
+    };
 
-  $timeout(_init.bind(this), 4);
+    var _getNotSelected = function () {
+      return existingRequirements.filter(function (currentRequirement) {
+        return !currentRequirement.isSelected;
+      });
+    };
 
-  var _getAllSelected = function () {
-    return existingRequirements.filter(function (currentRequirement) {
-      return currentRequirement.isSelected;
-    });
-  };
+    this.returnAllSelections = function () {
+      var allSelected = _getAllSelected();
 
-  var _getNotSelected = function () {
-    return existingRequirements.filter(function (currentRequirement) {
-      return !currentRequirement.isSelected;
-    });
-  };
+      $mdDialog.hide(allSelected);
+    };
 
-  this.returnAllSelections = function () {
-    var allSelected = _getAllSelected();
+    this.returnChangedSelections = function () {
+      var added = _.difference(_getAllSelected(), this.preSelectedRequirements);
+      var removed = _.difference(_getNotSelected(), this.preSelectedRequirements);
 
-    $mdDialog.hide(allSelected);
-  };
+      return {
+        added: added,
+        removed: removed
+      }
+    };
 
-  this.returnChangedSelections = function () {
-    var added = _.difference(_getAllSelected(), this.preSelectedRequirements);
-    var removed = _.difference(_getNotSelected(), this.preSelectedRequirements);
+    this.cancel = function () {
+      $mdDialog.cancel();
+    };
 
-    return {
-      added: added,
-      removed: removed
-    }
-  };
-
-  this.cancel = function () {
-    $mdDialog.cancel();
-  };
-
-}]);
+  }]);
