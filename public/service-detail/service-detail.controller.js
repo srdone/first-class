f3d33b32654e2aa188615083628b6aaef25900c7
Stephen Done <srdone@gmail.com>
2015-03-17 20:33:06 -0600

    reformatting

--- public/service-detail/service-detail.controller.js
+++ public/service-detail/service-detail.controller.js
@@ -3,37 +3,37 @@
 angular.module('firstClass').controller('ServiceDetailController', ['$mdToast', '$mdDialog', 'serviceProjectDialogService',
   function ($mdToast, $mdDialog, serviceProjectDialogService) {
 
-  var vm = this;
+    var vm = this;
 
     var warningDialog = $mdDialog.confirm().title('Warning').ok('Yes').cancel('Cancel');
 
-  debugger;
-  vm.editServiceProject = function (event, serviceProject) {
-    var serviceProjectToEdit = angular.copy(serviceProject);
+    debugger;
+    vm.editServiceProject = function (event, serviceProject) {
+      var serviceProjectToEdit = angular.copy(serviceProject);
 
-    serviceProjectDialogService.showEditDialog({targetEvent: event, serviceProject: serviceProjectToEdit}).then(function (editedServiceProject) {
-      vm.scout.removeService(serviceProject.id);
-      vm.scout.addService(editedServiceProject.description, editedServiceProject.hours);
-      vm.scout.save().then(function (savedScout) {
-        $mdToast.showSimple('Saved Changes: ' + editedServiceProject.toString());
-        vm.scout = savedScout;
-      });
-    });
-  };
-
-  vm.deleteService = function (service) {
-    var dialog = warningDialog.content('Delete service record: ' + service.description + ': ' + service.hours + ' hours?');
-
-    $mdDialog.show(dialog).then(function () {
-      vm.scout.removeService(service.id);
-      vm.scout.save()
-        .then(function () {
-          $mdToast.showSimple('Deleted service project: ' + service.toString());
-        }, function () {
-          vm.scout.addService(service);
-          $mdToast.showSimple('A server error occurred: Failed to delete service project');
+      serviceProjectDialogService.showEditDialog({targetEvent: event, serviceProject: serviceProjectToEdit}).then(function (editedServiceProject) {
+        vm.scout.removeService(serviceProject.id);
+        vm.scout.addService(editedServiceProject.description, editedServiceProject.hours);
+        vm.scout.save().then(function (savedScout) {
+          $mdToast.showSimple('Saved Changes: ' + editedServiceProject.toString());
+          vm.scout = savedScout;
         });
-    });
-  };
+      });
+    };
 
-}]);
+    vm.deleteService = function (service) {
+      var dialog = warningDialog.content('Delete service record: ' + service.description + ': ' + service.hours + ' hours?');
+
+      $mdDialog.show(dialog).then(function () {
+        vm.scout.removeService(service.id);
+        vm.scout.save()
+          .then(function () {
+            $mdToast.showSimple('Deleted service project: ' + service.toString());
+          }, function () {
+            vm.scout.addService(service);
+            $mdToast.showSimple('A server error occurred: Failed to delete service project');
+          });
+      });
+    };
+
+  }]);
