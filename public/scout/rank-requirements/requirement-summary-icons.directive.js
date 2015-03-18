d9e77a923f96382b7e5cedd2ff268b096b7e6542
Stephen Done <srdone@gmail.com>
2015-03-18 16:48:08 -0600

    fixed small display issue with requirement summary

--- public/scout/rank-requirements/requirement-summary-icons.directive.js
+++ public/scout/rank-requirements/requirement-summary-icons.directive.js
@@ -4,12 +4,7 @@ angular.module('firstClass').directive('fcsRequirementSummaryIcons', function ()
     scope: {
       reqCategoryCount: '='
     },
-    templateUrl: 'scout/rank-requirements/requirement-summary-icons.template.html',
-    link: function ($scope, $element, $attributes) {
-      if (_.isEmpty($scope.reqCategoryCount)) {
-        $element.hide();
-      }
-    }
+    templateUrl: 'scout/rank-requirements/requirement-summary-icons.template.html'
   }
 
 });
