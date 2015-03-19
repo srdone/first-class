0af6f41ece2c2eb1695ae64e28277687a8015995
Stephen Done <srdone@gmail.com>
2015-03-19 14:15:08 -0600

    make d3 chart dynamic

--- public/troop-progress-chart/troop-progress-chart.directive.js
+++ public/troop-progress-chart/troop-progress-chart.directive.js
@@ -17,6 +17,7 @@ angular.module('firstClass').directive('fcsTroopProgressChart', function () {
       var barPadding = 1;
 
       var renderChart = function () {
+
         var progress = $scope.troop.map(function (currentScout) {
           var name = currentScout.getName();
           var percentProgress = currentScout.getPercentProgressToFirstClass();
@@ -35,16 +36,17 @@ angular.module('firstClass').directive('fcsTroopProgressChart', function () {
           return currentScout.name;
         });
 
-        svg.selectAll('rect')
-          .data(data)
-          .enter()
-          .append('rect')
-          .attr('x', function (d, i) {
-            return i * ($scope.width / data.length)
-          })
-          .attr('y', function (d) {
-            return $scope.height - d;
-          })
+        var rects = svg.selectAll('rect').data(data);
+
+
+        // d3 is weird, you have to call enter and exit separately, then make changes.
+        // otherwise you are only performing your changes to a subset of the data.
+        rects.enter().append('rect');
+
+        rects.exit().remove();
+
+        rects.attr('x', function (d, i) {return i * ($scope.width / data.length); })
+          .attr('y', function (d) { return $scope.height - d; })
           .attr('width', ($scope.width / data.length) - barPadding)
           .attr('height', function (d) {
             return d
